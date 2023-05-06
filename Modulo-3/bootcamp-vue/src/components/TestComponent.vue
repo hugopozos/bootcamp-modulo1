7cc35f619a8877350ff38feef4a374051ddb521c
Hugo <pozos_herrera@outlook.es>
2023-05-05 18:31:56 -0600

    Proyecto de vue

---
+++ Modulo-3/bootcamp-vue/src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "TestComponent",
+    data() {
+        return {
+            something: 'Probando'
+        }
+    }
+})
+</script>
+
+<template>
+  <div>
+      <h1>Test Component</h1>
+      <p>{{ something }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
